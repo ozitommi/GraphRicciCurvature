24d6f77cdda3b098800e10b7bafaec7ca66eec5d
Tommi Muller <33109560+ozitommi@users.noreply.github.com>
2022-03-15 21:10:54 +0000

    Update OllivierRicci.py

--- GraphRicciCurvature/OllivierRicci.py
+++ GraphRicciCurvature/OllivierRicci.py
@@ -675,7 +675,7 @@ class OllivierRicci:
 
         """
         
-        print("Hello world! This is Tommi 1")
+        print("Hello world! This is Tommi Muller")
         
         self.G = G.copy()
         self.alpha = alpha
